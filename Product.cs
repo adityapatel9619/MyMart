ef9a97985f6f908aba47202f54080223f35a8630
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-08-03 17:01:30 +0530

    2

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMart
+{
+    class Product
+    {
+
+        //private int categoryId,productId;
+
+        //private string productName, productDescription, brand,manufacturedBy,createdOn,createdBy;
+
+        //private double mrp, discount, ourprice;      
+
+        private int categoryId, productId;
+        private string pName, pCode, pDescrp,brand;
+        private int mrp, discount, ourprice; 
+    }
+}
